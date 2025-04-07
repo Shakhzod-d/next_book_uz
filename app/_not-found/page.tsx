f3a976e99607c28951efa4e2e0e96f0b64514dab
Shakhzod <forworkingappsinmylaptop@gmail.com>
2025-04-07 10:16:18 +0500

    remove @testing-library/react

--- app/_not-found/page.tsx
+++ app/_not-found/page.tsx
@@ -1,33 +1,24 @@
-// app/_not-found/page.tsx
-"use client"; // Mark the component as client-side only
-
-import React, { Suspense } from "react";
-import { Loader } from "@/components"; // Assuming you have a loader component
-
-// If you're using useSearchParams, ensure it's wrapped in Suspense
+import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
 
 const NotFoundPage = () => {
   const searchParams = useSearchParams();
+  const [query, setQuery] = useState<any>(null);
+
+  useEffect(() => {
+    // Run only on the client side
+    const queryParam = searchParams.get("query");
+    setQuery(queryParam);
+  }, [searchParams]);
+
+  if (query === null) return <div>Loading...</div>;
 
   return (
     <div>
       <h1>404 - Page Not Found</h1>
-      <p>Sorry, we couldn't find the page you're looking for.</p>
-      <p>Search Params: {searchParams?.toString()}</p>{" "}
-      {/* Example usage of search params */}
+      <p>Search Query: {query}</p>
     </div>
   );
 };
 
-const NotFoundWrapper = () => {
-  return (
-    <Suspense fallback={<Loader />}>
-      {" "}
-      {/* Wrap in Suspense */}
-      <NotFoundPage />
-    </Suspense>
-  );
-};
-
-export default NotFoundWrapper;
+export default NotFoundPage;
